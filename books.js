63c5db238ae2a401042975de416323d399fda8a0
Craig <craig@superpoweredgames.com>
2024-06-26 12:59:34 +0100

    Basic card creation implemented

--- books.js
+++ books.js
@@ -1,8 +1,10 @@
 const submitButton = document.getElementById('submit');
-let bookName = document.getElementById('name');
-let bookAuthor = document.getElementById('author');
-let bookPages = document.getElementById('pages');
-let bookRead = document.getElementById('read');
+const bookName = document.getElementById('name');
+const bookAuthor = document.getElementById('author');
+const bookPages = document.getElementById('pages');
+const bookRead = document.getElementById('read');
+
+const cardContainer = document.getElementsByClassName('container')[0];
 
 class Book {
     constructor(title, author, pages, read) {
@@ -13,19 +15,54 @@ class Book {
     }
 };
 
-const myLibrary = [];
+let myLibrary = [];
 
 submitButton.addEventListener("click", (event) => {
     event.preventDefault();
-    addBookToLibrary(bookAuthor.value, bookAuthor.value, bookPages.value, bookRead.checked);
+    addBookToLibrary(bookName.value, bookAuthor.value, bookPages.value, bookRead.checked);
 });
 
 function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(new Book(title, author, pages, read));
-    createNewCard();
-    console.table(myLibrary);
+    updateLibrary();
 }
 
-function createNewCard() {
+function updateLibrary(){
+    //Remove all cards
+    while(cardContainer.firstChild){
+        cardContainer.removeChild(cardContainer.lastChild);
+    }
 
+    //Create new cards
+    myLibrary.forEach(book => {
+        createNewCard(book);
+    });
+}
+
+function createNewCard(bookNumber) {
+    const newCard = document.createElement("div");
+    newCard.className = "card";
+
+    const newBookName = document.createElement("h3");
+    const newBookAuthor = document.createElement("p");
+    const newBookPages = document.createElement("p");
+    const readLabel = document.createElement("label");
+    const newBookRead = document.createElement("input");
+    newBookRead.type = "checkbox";
+    newBookRead.id = 'booknum' + bookNumber;
+
+    newCard.appendChild(newBookName);
+    newCard.appendChild(newBookAuthor);
+    newCard.appendChild(newBookPages);
+    newCard.appendChild(readLabel);
+    newCard.appendChild(newBookRead);
+
+    cardContainer.appendChild(newCard);
+
+    newBookName.innerHTML = bookNumber.title;
+    newBookAuthor.innerHTML = bookNumber.author
+    newBookPages.innerHTML = bookNumber.pages
+    newBookRead.checked = bookNumber.read;
+    readLabel.innerHTML = "Read Status?"
+    newBookRead.disabled = true;
 }
