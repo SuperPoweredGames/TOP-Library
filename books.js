18206ce2058f4e2be8807e145ad4e9a1a60b6e0a
Craig <craig@superpoweredgames.com>
2024-06-26 11:55:13 +0100

    Basic framework of page, basic form functionality

--- books.js
+++ books.js
@@ -1,18 +1,31 @@
+const submitButton = document.getElementById('submit');
+let bookName = document.getElementById('name');
+let bookAuthor = document.getElementById('author');
+let bookPages = document.getElementById('pages');
+let bookRead = document.getElementById('read');
+
 class Book {
     constructor(title, author, pages, read) {
         this.title = title;
         this.author = author;
         this.pages = pages;
         this.read = read;
-        this.info = function() {
-            let haveRead = read ? 'have read' : 'have not read yet';
-            return `${title} by ${author}, ${pages} pages, ${haveRead}`;
-        }
     }
 };
 
 const myLibrary = [];
 
-function addBookToLibrary() {
-    
+submitButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    addBookToLibrary(bookAuthor.value, bookAuthor.value, bookPages.value, bookRead.checked);
+});
+
+function addBookToLibrary(title, author, pages, read) {
+    myLibrary.push(new Book(title, author, pages, read));
+    createNewCard();
+    console.table(myLibrary);
+}
+
+function createNewCard() {
+
 }
